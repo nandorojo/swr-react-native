ba6be0f76f2c66cc28bedcb2a79ca9cdfea12708
Fernando Rojo <frojo@sas.upenn.edu>
2020-12-02 13:35:22 -0500

    🕸 test fixes

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,9 @@
-import 'react-app-polyfill/ie11';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import 'react-app-polyfill/ie11'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 
 const App = () => {
-  return (
-    <div>
-      <Thing />
-    </div>
-  );
-};
+  return <div></div>
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
